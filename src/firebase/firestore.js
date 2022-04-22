49ab99b39ebd2bfe239b1215b895d53cb2c5362d
Felipe-Gabriel-dos-Santos <vdoss2011@gmail.com>
2022-04-22 17:17:54 -0300

    old JavaScript code

--- src/firebase/firestore.js
+++
@@ -1,6 +0,0 @@
-import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
-import app from "./firebaseConfig.js";
-
-export const Firestore = getFirestore(app);
-export const Auth = getAuth(app);
