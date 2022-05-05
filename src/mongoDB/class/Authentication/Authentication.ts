d7cfcca381c28bdcf15f09555822d05474eb1a3f
Felipe-Gabriel-dos-Santos <vdoss2011@gmail.com>
2022-05-04 22:51:36 -0300

    improvements to authentication functions

--- src/mongoDB/class/Authentication/Authentication.ts
+++ src/mongoDB/class/Authentication/Authentication.ts
@@ -1,54 +1,64 @@
-// import { Users } from "../Users";
-// import userModel from "../../../mongoDB/schemas/user";
-// import { JWT } from "./JWT";
+import { Users } from "../Users";
+import userModel from "../../../mongoDB/schemas/user";
+import { JWT } from "./JWT";
+import { IUser } from "../../../types/user";
 
-// class Authentication {
+class Authentication {
+  constructor(
+    private readonly email: string,
+    private readonly password: string,
+    private JWTAuthentication = new JWT(),
+    private userCollection = new Users(userModel),
+    private isLoggedIn: Boolean = false
+  ) {}
 
-//   constructor(
-//     private readonly email: string,
-//     private readonly password: string,
-//     private JWTAuthentication = new JWT(),
-//     private userCollection = new Users(userModel),
-//     private isLoggedIn: Boolean = false
-//   ) {}
+  login(): Promise<IUser> {
+    return new Promise((resolve, reject) => {
+      this.userCollection
+        .readUserByFields({ email: this.email, password: this.password })
+        .then((user) => {
+          this.isLoggedIn = true;
+          const loggedUser = user[0];
 
-//   login() {
-//     return new Promise((resolve, reject) => {
-//       this.userCollection
-//         .({ email: this.email, password: this.password })
-//         .then((user) => {
-//           this.isLoggedIn = true;
-//           user.token = this.JWTAuthentication.generateToken(user.id);
-//           resolve(user);
-//         })
-//         .catch((err) => {
-//           this.isLoggedIn = false;
-//           reject(err);
-//         });
-//     });
-//   }
+          this.JWTAuthentication.generateToken(loggedUser.id)
+            .then((token) => (loggedUser.token = token))
+            .catch((err) => err);
 
-//   createAccount(name: string) {
-//     return this.userCollection
-//       .createDocument({
-//         name,
-//         email: this.email,
-//         password: this.password,
-//       })
-//       .then((user) => {
-//         this.isLoggedIn = true;
-//         user.token = this.JWTAuthentication.generateToken(user.id);
-//         return user;
-//       })
-//       .catch((err) => {
-//         this.isLoggedIn = false;
-//         return err;
-//       });
-//   }
+          resolve(loggedUser);
+        })
+        .catch((err) => {
+          this.isLoggedIn = false;
+          reject(err);
+        });
+    });
+  }
 
-//   isLogged() {
-//     return this.isLoggedIn;
-//   }
-// }
+  createAccount(name: string): Promise<IUser> {
+    return new Promise((resolve, reject) => {
+      this.userCollection
+        .createUser({
+          name,
+          email: this.email,
+          password: this.password,
+        })
+        .then((user) => {
+          this.isLoggedIn = true;
+          this.JWTAuthentication.generateToken(user.id)
+            .then((token) => (user.token = token))
+            .catch((err) => reject(err));
 
-// export { Authentication };
+          resolve(user);
+        })
+        .catch((err) => {
+          this.isLoggedIn = false;
+          reject(err);
+        });
+    });
+  }
+
+  isLogged() {
+    return this.isLoggedIn;
+  }
+}
+
+export { Authentication };
