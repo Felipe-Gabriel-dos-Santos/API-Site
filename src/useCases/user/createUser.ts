f403d7f913d06e676406a37b6e56b967939af4f5
Felipe-Gabriel-dos-Santos <vdoss2011@gmail.com>
2022-05-19 21:20:54 -0300

    create user useCase

---
+++ src/useCases/user/createUser.ts
@@ -0,0 +1,11 @@
+import UserRepository from "../../repositories/userRepository/userRepository";
+
+import { User } from "../../domain/entities/user";
+
+class createUser {
+  constructor(private userRepository: UserRepository) {}
+
+  async execute(data: User): Promise<User> {
+    return await this.userRepository.create(data);
+  }
+}
